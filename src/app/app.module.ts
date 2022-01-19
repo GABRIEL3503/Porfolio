c9830d3b73664281d162352eb157b4f86bdf3fe4
gabriel <you@example.com>
2022-01-19 16:45:42 -0300

    cree un componente para cada seccion de la maqueta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { FormacionComponent } from './components/formacion/formacion.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    AcercaDeComponent,
+    FormacionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
